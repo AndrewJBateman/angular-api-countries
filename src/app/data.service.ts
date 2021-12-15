f85fa76507ac6c659adb29fe728d6f050e11cc6a
AndrewJBateman <gomezbateman@yahoo.com>
2021-12-15 19:04:59 +0100

    fix: updated to new API addressl & check results

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
 })
 export class DataService {
-  API_URL = "https://restcountries.eu/rest/v2";
+  API_URL = "https://restcountries.com/v3.1";
   result: any;
 
   constructor(private http: HttpClient) {}
 
   // function to get info for all countries
   getCountries() {
-    return this.http
-      .get(`${this.API_URL}/all`)
-      .pipe(map((result) => (this.result = result)));
+    return this.http.get(`${this.API_URL}/all`).pipe(
+      tap((result) => console.log("result", result)),
+      map((result) => (this.result = result))
+    );
   }
 }
